e87a42a2ac1bea31f10498547d13e3eb3210788c
rishu-29x <rishu29x@gmail.com>
2026-06-29 01:30:15 +0530

    feat: add blink counter and structured output to LED blink v1.3
    Added a blink counter and improved program structure.

--- IoT Week1/LED Blink/code.ino
+++ IoT Week1/LED Blink/code.ino
@@ -1,18 +1,24 @@
-// LED Blink Program - v1.2
-// Description: Added Serial Monitor feedback
+// LED Blink Program - v1.3
+// Description: Added blink counter and improved structure
 
 const int LED_PIN = 13;
 const int BLINK_DELAY = 1000;
 
+int blinkCount = 0;
+
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   Serial.begin(9600);
-  Serial.println("LED Blink Program v1.2 Started");
+  Serial.println("LED Blink Program v1.3 Started");
 }
 
 void loop() {
+  blinkCount++;
+
   digitalWrite(LED_PIN, HIGH);
-  Serial.println("LED ON");
+  Serial.print("Blink #");
+  Serial.print(blinkCount);
+  Serial.println(" - LED ON");
   delay(BLINK_DELAY);
 
   digitalWrite(LED_PIN, LOW);
