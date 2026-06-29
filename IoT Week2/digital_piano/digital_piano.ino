437181563e8968118aaf0b351833b6d860f0f5f4
rishu-29x <rishu29x@gmail.com>
2026-06-29 05:32:28 +0530

    feat: add mode toggle button for Major and Minor scale
    Added mode toggle functionality to switch between major and minor scales.

--- IoT Week2/digital_piano/digital_piano.ino
+++ IoT Week2/digital_piano/digital_piano.ino
@@ -7,13 +7,24 @@ const int BTN1 = 2;  // Do
 const int BTN2 = 3;  // Re
 const int BTN3 = 4;  // Mi
 const int BTN4 = 5;  // Fa
+const int BTN_MODE = 6;  // Mode toggle
 const int BUZZER = 9;
+
+// Major scale frequencies
+const int majorFreqs[4] = {262, 294, 330, 349};
+
+// Minor scale frequencies
+const int minorFreqs[4] = {262, 277, 311, 349};
+
+bool isMajor = true;
+bool lastModeState = LOW;
 
 void setup() {
   pinMode(BTN1, INPUT);
   pinMode(BTN2, INPUT);
   pinMode(BTN3, INPUT);
   pinMode(BTN4, INPUT);
+  pinMode(BTN_MODE, INPUT);
   pinMode(BUZZER, OUTPUT);
 }
 
@@ -22,27 +33,19 @@ void loop() {
   bool b2 = digitalRead(BTN2);
   bool b3 = digitalRead(BTN3);
   bool b4 = digitalRead(BTN4);
+  bool bMode = digitalRead(BTN_MODE);
+
+  // Mode toggle - only triggers once per press
+  if (bMode == HIGH && lastModeState == LOW) {
+    isMajor = !isMajor;
+    delay(200);  // debounce
+  }
+  lastModeState = bMode;
+
+  // Pick frequency array based on current mode
+  const int* freqs = isMajor ? majorFreqs : minorFreqs;
 
   int pressedCount = b1 + b2 + b3 + b4;
 
   if (pressedCount >= 2) {
-    tone(BUZZER, 392);  // Sol - chord substitute
-  }
-  else if (b1) {
-    tone(BUZZER, 262);  // Do
-  }
-  else if (b2) {
-    tone(BUZZER, 294);  // Re
-  }
-  else if (b3) {
-    tone(BUZZER, 330);  // Mi
-  }
-  else if (b4) {
-    tone(BUZZER, 349);  // Fa
-  }
-  else {
-    noTone(BUZZER);     // silence when no button pressed
-  }
-
-  delay(10);
-}
+    tone(BUZZER, 392);  // Sol -
